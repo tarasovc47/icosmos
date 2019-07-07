9c26fe909f1d50311ab7b1175b39cee78b411b41
tarasovc47 <tarasovc47@live.ru>
2019-07-07 16:18:03 +0500

    Добавлена модель "client" и CRUD для неё

---
+++ views/client/index.php
@@ -0,0 +1,40 @@
+<?php
+
+use yii\helpers\Html;
+use yii\grid\GridView;
+
+/* @var $this yii\web\View */
+/* @var $searchModel app\models\ClientSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Clients';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="client-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?= Html::a('Create Client', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'id',
+            'client_name',
+            'client_email:email',
+            'client_phone',
+            'client_message',
+
+            ['class' => 'yii\grid\ActionColumn'],
+        ],
+    ]); ?>
+
+
+</div>
