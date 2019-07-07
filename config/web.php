30b6164c47571bb27d1a06fc6f054d42ef0f27a7
tarasovc47 <tarasovc47@live.ru>
2019-07-07 15:37:08 +0500

    Revert "добавлен модуль админки"
    This reverts commit ded262c6cef1b7cc703635311e56072b1500e1db.

--- config/web.php
+++ config/web.php
@@ -51,11 +51,6 @@ $config = [
             ],
         ],
     ],
-    'modules' => [
-        'admin' => [
-            'class' => 'app\modules\admin\Module',
-        ],
-    ],
     'params' => $params,
 ];
 
